cfa2b59e1edd92843131481bf278c90072503a6a
alejandro <alexandros2707@gmail.com>
2021-12-17 21:21:41 -0400

    🧰 change backend urls and improve seed-data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,29 +1,42 @@
 import {config} from '@keystone-6/core'
 import {lists} from './schema'
 import {withAuth, session} from './auth'
+import {BaseKeystoneTypeInfo, KeystoneContext} from '@keystone-6/core/types'
+import {insertSeedData} from './seed-data/seed'
 
 const databaseURL = process.env.DATABASE_URL || 'file:./keystone.db'
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:4444'
+const frontendURL = process.env.FRONTEND_URL || 'http://localhost:4444'
+
+type OnConnect =
+  | ((args: KeystoneContext<BaseKeystoneTypeInfo>) => Promise<void>)
+  | undefined
+
+const onConnect: OnConnect = async context => {
+  console.log('Connected to the database! 🔌 🗃️')
+  if (process.argv.includes('--seed-data')) {
+    await insertSeedData(context)
+  }
+}
 
 export default withAuth(
   config({
     server: {
+      port: 4444,
       cors: {
-        origin: [frontendUrl],
+        origin: [frontendURL],
         credentials: true,
       },
     },
     db: process.env.DATABASE_URL
-      ? {provider: 'postgresql', url: process.env.DATABASE_URL}
+      ? {
+          provider: 'postgresql',
+          url: process.env.DATABASE_URL,
+          onConnect,
+        }
       : {
           provider: 'sqlite',
           url: databaseURL,
-          async onConnect(context) {
-            console.log('Connected to the database!')
-            if (process.argv.includes('--seed-data')) {
-              // await insertSeedData(context)
-            }
-          },
+          onConnect,
         },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
